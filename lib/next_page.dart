fc72d2b2f4374f69c85685581c886a6dd8e6844f
広田　響 <35335011+teimusheru@users.noreply.github.com>
2022-07-31 09:59:32 +0900

    Flutter  画面遷移

---
+++ lib/next_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('次の画面'),
+      ),
+      body: Container(
+          height: double.infinity,
+          color: Colors.red,
+          child: Center(
+             child: RaisedButton(
+               child: Text('戻る'),
+             onPressed: () {
+               Navigator.pop(context);
+
+        },
+    ),
+    ),
+      ),
+    );
+}
+}
